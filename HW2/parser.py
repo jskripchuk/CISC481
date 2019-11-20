c15ba7c4377d85357797c46280703cdf2bcf3ac9
Jimmy <jimmyskripchuk@gmail.com>
2019-11-20 09:19:43 -0500

    almost done

---
+++ HW2/parser.py
@@ -0,0 +1,89 @@
+#STolen from
+#https://norvig.com/lispy.html
+
+Symbol = str              # A Scheme Symbol is implemented as a Python str
+Number = (int, float)     # A Scheme Number is implemented as a Python int or float
+Atom   = (Symbol, Number) # A Scheme Atom is a Symbol or Number
+List   = list             # A Scheme List is implemented as a Python list
+Exp    = (Atom, List)     # A Scheme expression is an Atom or List
+Env    = dict             # A Scheme environment (defined below) 
+                          # is a mapping of {variable: value}
+                          
+
+def tokenize(chars: str) -> list:
+    "Convert a string of characters into a list of tokens."
+    return chars.replace('(', ' ( ').replace(')', ' ) ').split()
+
+def parse(program: str) -> Exp:
+    "Read a Scheme expression from a string."
+    return read_from_tokens(tokenize(program))
+
+def read_from_tokens(tokens: list) -> Exp:
+    "Read an expression from a sequence of tokens."
+    if len(tokens) == 0:
+        raise SyntaxError('unexpected EOF')
+    token = tokens.pop(0)
+    if token == '(':
+        L = []
+        while tokens[0] != ')':
+            L.append(read_from_tokens(tokens))
+        tokens.pop(0) # pop off ')'
+        return L
+    elif token == ')':
+        raise SyntaxError('unexpected )')
+    else:
+        return atom(token)
+
+def atom(token: str) -> Atom:
+    "Numbers become numbers; every other token is a symbol."
+    try: return int(token)
+    except ValueError:
+        try: return float(token)
+        except ValueError:
+            return Symbol(token)
+
+
+    #res = [convert(l) for l in res]
+    #print(res)
+
+
+
+#print(parse_parentheses('a(b(cd)f)')) # ['a', ['b', ['c', 'd'], 'f']]
+
+def pred(name, var_list):
+    return {
+        "name": name,
+        "vars": var_list
+    }
+
+def lst_to_pred(lst):
+    name = lst[0]
+    v = lst[1:]
+    return pred(name,v)
+
+
+def parse_file(filename):
+    f = open(filename)
+    a = f.read()
+    f.close()
+
+    a = a.split("\n\n")  
+
+    
+        
+    kb_parse = parse(a[0])
+    kb_name = kb_parse[1]
+    kb = kb_parse[3:][0]
+
+    CLAUSES = []
+    for clause in kb:
+        current_clause = [lst_to_pred(x) for x in clause]
+        CLAUSES.append(current_clause)
+
+    goal = lst_to_pred(parse(a[1])[3][0])
+
+    return CLAUSES, goal, kb_name
+
+    #print(CLAUSES)
+        #print(clause)  
+
--- HW2/resolver.py
+++ HW2/resolver.py
@@ -1,66 +1,14 @@
 import uuid
 
+MAX_DEPTH = 300
+
 def pred(name, *var_list):
     return {
         "name": name,
         "vars": list(var_list)
     }
 
-KB_1 = [
-    [pred("grandparent", "?x", "?y"), pred("parent", "?x", "?z"), pred("parent", "?z", "?y")],
-    [pred("parent", "Brian", "Doug")],
-    [pred("parent", "Doug", "Mary")],
-    [pred("parent", "Mary", "Sue")]
-]
 
-GOAL_1 = pred("grandparent", "?x1", "?y1")
-
-KB_2 = [
-    [pred("aunt","?x","?y"), pred("sister","?x","?z"), pred("mother","?z","?y")],
-    [pred("aunt","?x","?y"), pred("sister","?x","?z"), pred("father","?z","?y")],
-    [pred("sister","Mary","Sue")],
-    [pred("sister","Mary","Doug")],
-    [pred("father","Doug","John")],
-    [pred("mother","Sue","Paul")]
-]
-
-GOAL_2 = pred("aunt","?x","?y")
-
-KB_3 = [
-    [pred("simple_sentence", "?x", "?y", "?z", "?u" ,"?v"),
-     pred("noun_phrase", "?x", "?y"),
-     pred("verb_phrase", "?z", "?u", "?v")],
-
-    [pred("noun_phrase", "?x", "?y"), pred("article", "?x"), pred("noun", "?y")],
-    [pred("verb_phrase","?x","?y","?z"), pred("verb","?x"), pred("noun_phrase","?y","?z")],
-    [pred("article","a")],
-    [pred("article","the")],
-    [pred("noun","man")],
-    [pred("noun","dog")],
-    [pred("verb","likes")],
-    [pred("verb","bites")]
-
-    ]
-
-GOAL_3 = pred("simple_sentence","?x","?y","?z","?u","?v")
-
-KB_4 = [
-    [pred("ancestor", "?x","?y"), pred("parent","?x","?y")],
-    [pred("ancestor","?x","?y"), pred("ancestor","?x","?z"), pred("parent","?z","?y")],
-    [pred("parent","Mary","Paul")],
-    [pred("parent","Sue","Mary")]
-]
-
-GOAL_4 = pred("ancestor","?x","?y")
-
-KB_5 = [
-    [pred("ancestor","?x","?y"), pred("ancestor","?x","?z"), pred("parent","?z","?y")],
-    [pred("ancestor", "?x","?y"), pred("parent","?x","?y")],
-    [pred("parent","Mary","Paul")],
-    [pred("parent","Sue","Mary")]
-]
-
-GOAL_5 = pred("ancestor","?x","?y")
 
 
 def binding(v, bindings):
@@ -264,14 +212,7 @@ pp = pprint.PrettyPrinter(indent=4)
 
 import queue
 
-def resolve(kb, goal, mode="dfs",max_depth=300):
-    print("MODE",mode)
-    #print(kb)
-
-    #print(kb)
-
-    
-    #bindings = {}
+def resolve(kb, goal, mode="dfs",max_depth=MAX_DEPTH,infinite_loop=True):
     if mode == "dfs":
         open_list = []
         open_list.append( ([goal], {}) )
@@ -280,67 +221,26 @@ def resolve(kb, goal, mode="dfs",max_depth=300):
         open_list.put( ([goal], {}) )
 
     i=0
-    #pp.pprint(open_list)
-    print("----")
-
     successes = []
 
     while open_list:
-        #print("LOOP",i)
-        #print("OPEN LIST")
-
-        #pp.pprint(open_list)
-
         kb = [uniqify(x) for x in kb]
-        #print(open_list.qsize())
-        #print(lst(open_list))
+
         if mode == "dfs":
             current_node = open_list.pop()
         else:
-            #print(open_list.queue)
             if open_list.qsize() == 0:
                 break
             current_node = open_list.get()
-            #print(current_node)
 
         current_clause = current_node[0]
         current_bindings = current_node[1]
 
-        """
-        print("CURRENT CLAUSE")
-        pp.pprint(current_clause)
-        print("CURRENT BINDINGS")
-        pp.pprint(current_bindings)
-        print()
-        """
-        
-
-        #to_resolve = kb[0][0]
-        #print("START RESOLVING")
         for clause in kb:
-            #pp.pprint(clause)
-            #print(current_clause)
-
-            #current_clause = uniqify(clause)
-            #clause = uniqify(clause)
-            #print(clause)
-            #print(current_clause)
             to_resolve = clause[0]
             predicate = current_clause[0]
-            #or predicate in current_clause:
             if can_resolve(predicate,to_resolve):
-                """
-                #print("RESOLVING",predicate,to_resolve)
-                #print("")
-                #print("RESOLVING")
-                pp.pprint(predicate)
-                #print("WITH")
-                pp.pprint(to_resolve)
-                #print("WITH BINDINGS")
-                pp.pprint(current_bindings)
-                """
                 
-                infinite_loop = False
 
                 if infinite_loop:
                     new_clause = copy.deepcopy(clause[1:])
@@ -349,21 +249,8 @@ def resolve(kb, goal, mode="dfs",max_depth=300):
                     new_clause = copy.deepcopy(current_clause[1:])
                     new_clause.extend(clause[1:])
                     
-
-                #pp.pprint(clause)
-               # print("RESULTING IN")
-                #pp.pprint(new_clause)
-                #print(clause[1:])
-                #print(current_clause[1:])
-                #print(new_clause)
-                #print(current_bindings)
                 unify_results = unify(predicate,to_resolve,current_bindings)
 
-                #print("UNIFY RESULT",unify_results)
-                #print("---------")
-                
-                #if unify_results == {}:
-                #    print("WE DID IT REDDIT")
                 if unify_results != None and new_clause != []:
                     if mode == "dfs":
                         open_list.append(
@@ -378,69 +265,86 @@ def resolve(kb, goal, mode="dfs",max_depth=300):
 
 
                 if new_clause == [] and unify_results != None:
-                #    print("WE REDDIT, DID IT")
                     successes.append(unify_results)
         
-
-        #print("YEET")
         if i > max_depth:
             print("ERR: Max Depth of",str(max_depth),"Reached")
             return None
         
         i = i+1
-        #input()
-        #print("#################")
-        #input()
-        #print("############")
 
-    #print("YEET")
+    return successes
 
-    #print("HUGE SUCCESS",successes)
-    
-    for i,bindings in enumerate(successes):
-        #print(bindings)
-        print("BINDING",i)
+def print_binding_list(binding_list, goal):
+    for i,bindings in enumerate(binding_list):
+        final_bindings = "["
         for var in goal["vars"]:
             b = binding(var, bindings)
-            print(var,"/",b)
+            final_bindings += (var+"/"+b+", ")
+            #print(var,"/",b)
 
-    return 0
+        final_bindings = final_bindings[0:-2]+"]"
+        print(final_bindings)
 
-def test_resolve():
-    
-    """
-    print("RESOLVE 1")
-    res = resolve(KB_1,GOAL_1,mode="bfs")
-    #print(res)
-    print("RESOLVE 2")
-    res = resolve(KB_2,GOAL_2,mode="bfs")
-    """
-    
-    
-    #print("RESOLVE 3")
-    #res = resolve(KB_3,GOAL_3)
+import parser
+import os
 
 
-    
+import argparse
 
-    print("RESOLVE 4")
-    res = resolve(KB_4,GOAL_4,mode="bfs")
+def resolve_file(filename, mode="dfs", max_depth=MAX_DEPTH):
+    search_dic = {"bfs": "Breadth First Search", "dfs": "Depth First Search"}
 
-    print("RESOLVE 5")
-    res = resolve(KB_5,GOAL_5,mode="bfs")
-    
-    #print(res)
-    #print(res)
+    PAGE_BREAK_SIZE = 40
+    print("#"*PAGE_BREAK_SIZE)
+    kb, g, name = parser.parse_file(filename)
+    print(name+":")
+    print("Resolving using",search_dic[mode])
+    print("-"*PAGE_BREAK_SIZE)
+
+
+    res = resolve(kb,g,mode,max_depth)
+    if res:
+        print_binding_list(res,g)
+    else:
+        print("Resolution Failed")
+    print("#"*PAGE_BREAK_SIZE)
+
+
+def test_resolve(mode="dfs",max_depth = 300):
+    folder = "./test_cases"
+    files = os.listdir(folder)
+
+    for f in files:
+        resolve_file(folder+"/"+f,mode,max_depth)
+        print()
+        print()
+
+import argparse
 
 def main():
-    #test_resolve()
-    #test_unify()
-    #test_unify2()
-    #test_bindings()
-    #uniqify("hi")
-    #test_uniqify()
-    print("-----")
-    test_resolve()
+    parser = argparse.ArgumentParser(description="Resolver")
+    parser.add_argument("--file", type=str)
+    parser.add_argument("--method", type=str)
+    parser.add_argument("--max_depth", type=int)
+    parser.add_argument("--test_cases", action="store_true")
+
+    args = parser.parse_args()
+
+    method = "dfs"
+    max_depth = 300
+    
+    if args.method:
+        method = args.method
+
+    if args.max_depth:
+        max_depth = args.max_depth
+
+    if args.test_cases:
+        test_resolve(method,max_depth)
+    elif args.file:
+        resolve_file(args.file, method,max_depth)
+
 
 if __name__ == "__main__":
     main()
